b3f1128eb2e4bb271f9eeec328dff44c6e763672
Rio Y Kurniawan <rioyulinanda@gmail.com>
2023-02-16 18:53:41 +0800

    adding back to top button, adding javascript

---
+++ index.js
@@ -0,0 +1,21 @@
+let mybutton = document.getElementById("myBtn");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 200 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    mybutton.style.display = "flex";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
