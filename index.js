119f871139aad8baaa6b79f90e35d1b25deac793
nikosetiawanp <nikosetiawanp@gmail.com>
2023-02-16 19:51:52 +0700

    fixed scrolling

--- index.js
+++ index.js
@@ -19,3 +19,38 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+
+// SMOOTH
+
+const allLinks = document.querySelectorAll('a:link');
+console.log(allLinks);
+
+allLinks.forEach(function (link) {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+    const href = link.getAttribute('href');
+
+    //Scroll back to top
+    if (href === "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      });
+    }
+
+    //scroll to other links
+    if (href !== "#" && href.startsWith('#')) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    //close mobile nav
+
+    if (link.classList.contains("main-nav-link")) {
+      if (headerEl.classList.contains("nav-open")) {
+        headerEl.classList.remove("nav-open");
+      }
+    }
+  });
+});
